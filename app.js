c257dd3fe8a0a6d4263b14f6a6d96edf34d58266
Maham Abdul Razzaque <mahamabdulrazzaque@gmail.com>
2024-10-28 03:18:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+let age = 17;
+
+alert("I am  " + age + "  years Old :");
+
+// EnD
+
+// Question samjh nhii Araha !!!
+//2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+
+// EnD
+
+// Declare a variable for birth year
+let birthYear = 2007;
+
+document.write("My Birth year is : " + birthYear , "<br>");
+
+// EnD
+"<br>"
+
+// Store visitor's information in variables
+let visitorName = "John Doe"; 
+let productTitle = "T-shirt";   
+let quantity = 10;         
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on Bad Boy Outfitters Clothing store.");
+
+// EnD
+
+
